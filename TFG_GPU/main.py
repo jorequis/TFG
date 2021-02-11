2cbfb3b0a6168c932a845004b11fc112abed3ef1
Jorge <jorgecarlin@gmail.com>
2021-02-11 17:31:19 +0100

    Separados los formatos de los archivos en el Main

--- TFG_GPU/main.py
+++ TFG_GPU/main.py
@@ -9,7 +9,12 @@ import p2cropsmooth
 import p3video
 
 #Mensaje con el modo de empleo
-usage = 'Modo de empleo: -v VÍDEO [OPCIÓN]... [FICHERO]...'
+USAGE = 'Modo de empleo: -v VÍDEO [OPCIÓN]... [FICHERO]...'
+
+#Extension de los archivos
+P1EXT = '.txt'
+P2EXT = '.txt'
+P3EXT = '.mp4'
 
 #Ruta del programa
 path = os.path.dirname(os.path.realpath(__file__)) + '/'
@@ -20,9 +25,9 @@ arguments = [
     ('-c', '--config',   False, 'path to yolo config file', path + '/p1yolo/cfg/yolov3-tiny.cfg'),
     ('-w', '--weights',  False, 'path to yolo pre-trained weights', path + '/p1yolo/weights/yolov3-tiny.weights'),
     ('-cl', '--classes', False, 'path to text file containing class names', path + '/p1yolo/cfg/coco.data'),
-    ('-o1', '--output1', False, 'path to text file containing class names', path + '/p1_output_from_main.txt'),
-    ('-o2', '--output2', False, 'path to text file containing class names', path + '/p2_output_from_main.txt'),
-    ('-o3', '--output3', False, 'path to text file containing class names', path + '/p3_output_from_main.avi')
+    ('-o1', '--output1', False, 'path to text file containing class names', path + '/p1_output_from_main'),
+    ('-o2', '--output2', False, 'path to text file containing class names', path + '/p2_output_from_main'),
+    ('-o3', '--output3', False, 'path to text file containing class names', path + '/p3_output_from_main')
 ]
 
 #Ejecuta el programa principal
@@ -34,32 +39,32 @@ def main():
     args.config = os.path.realpath(args.config)
     args.weights = os.path.realpath(args.weights)
     args.classes = os.path.realpath(args.classes)
-    args.output1 = os.path.realpath(args.output1)
-    args.output2 = os.path.realpath(args.output2)
-    args.output3 = os.path.realpath(args.output3)
+    args.output1 = os.path.realpath(args.output1) + P1EXT
+    args.output2 = os.path.realpath(args.output2) + P2EXT
+    args.output3 = os.path.realpath(args.output3) + P3EXT
 
     #Cambiamos el directorio de trabajo a la ruta del programa para que funcionen las rutas relativas
     os.chdir(path)
 
-    p1_file = p1yolo.execute(args.video, args.config, args.weights, args.classes, args.output1, True)
+    #p1_code = p1yolo.execute(args.video, args.config, args.weights, args.classes, args.output1, True)
 
-    p2_file = p2cropsmooth.execute(args.output1, args.output2, args.video)
+    p2_code = p2cropsmooth.execute(args.output1, args.output2, args.video)
     
-    #p3video.execute(args.video, p2_file)
+    p3video.execute(args.video, args.output2, 720, 480, args.output3)
 
 #Obtiene los argumentos definidos
 def get_arguments():    
     #Inicializamos
-    ap = argparse.ArgumentParser()
+    argument_parser = argparse.ArgumentParser()
     #Sobreescribimos la funcion que escribe un error por pantalla para que escriba lo que nosotros queremos
-    ap.error = lambda message: arguments_error(ap, message)
-    ap.print_help = lambda file=None: print_help()
+    argument_parser.error = lambda message: arguments_error(argument_parser, message)
+    argument_parser.print_help = lambda file=None: print_help()
 
     #Definimos los argumentos
     for argument in arguments:
-        ap.add_argument(argument[0], argument[1], required=argument[2], help=argument[3], default=argument[4])
+        argument_parser.add_argument(argument[0], argument[1], required=argument[2], help=argument[3], default=argument[4])
 
-    return ap.parse_args()
+    return argument_parser.parse_args()
 
 #Escribe por pantalla el error que ha producido los argumentos
 def arguments_error(argument_parser, message):
@@ -68,7 +73,7 @@ def arguments_error(argument_parser, message):
 
 #Escribe por pantalla el uso del programa
 def print_help():
-    print(usage)
+    print(USAGE)
 
     print('\nArgumentos obligatorios')
     for argument in arguments:
